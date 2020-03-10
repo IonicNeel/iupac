3c42693d151c51d9abea441c7015ac28d77db6dd
IonicNeel <ionicneel@gmail.com>
2020-03-10 15:22:50 +0530

    Fixex some error and now works perfectly for anes,enes,ynes

--- app/src/main/java/com/example/iupactest/MainActivity.java
+++ app/src/main/java/com/example/iupactest/MainActivity.java
@@ -12,17 +12,20 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     String[][] grid = new String[3][10];
     char var2;
-    int resid,l=0;
+    int resid,l=0, fic,fjc,j;
     String ans,carbon,hydrogen,temp,var;
-    Button go;
+    Button go,clear;
     TextView answer;
     String[] wordroot = new String[12];
-    int carbons = 0, hydrogens = 0;
+    int carbons = 0, hydrogens = 0, oxygens =0;
+    Boolean found;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        found = false;
         setContentView(R.layout.activity_main);
         go = findViewById(R.id.go);
+        clear = findViewById(R.id.clear);
         answer = findViewById(R.id.answer);
         carbon = "C";
         hydrogen = "H";
@@ -47,27 +50,75 @@ public class MainActivity extends AppCompatActivity {
                         resid = getResources().getIdentifier(temp,"id", getPackageName());
                         EditText t = findViewById(resid);
                         var = ((EditText) findViewById(resid)).getText().toString();
-                        System.out.println("Value of \""+temp+"\" is: \""+var+"\"");
+//                        System.out.println("Value of \""+temp+"\" is: \""+var+"\"");
                         if(var.equals("C"))
-                            carbons++;
+                            carbons=carbons+1;
                         else if(var.equals("H"))
                             hydrogens++;
+                        else if(var.equals("O"))
+                            oxygens++;
+
+                        if(carbons == 1 && found.equals(false)){
+                            fic = i;
+                            fjc = j;
+                            found = true;
+                        }
+                        grid[i][j] = var;
+//                        System.out.println(temp+" - carbs- "+carbons);
                     }
                 }
-                System.out.println("Carbons = "+carbons);
+//                System.out.println("Carbons = "+carbons);
+                carbons=0;hydrogens=0;
+                System.out.println(fic+" "+fjc);
+                for(int j = fjc;j<9;j++){
+                    if(grid[fic][j].equals("C"))
+                        hydrogens+=0;
+                    else
+                        break;
+
+                    carbons++;
+                    if(grid[fic-1][j].equals("H"))
+                        hydrogens++;
+                    if(grid[fic+1][j].equals("H"))
+                        hydrogens++;
+                    if(grid[fic][j-1].equals("H"))
+                        hydrogens++;
+                    if(grid[fic][j+1].equals("H")){
+                        hydrogens++;
+                    }
+                }
+                System.out.println("Hy - "+hydrogens);
+
+
                 ans = wordroot[carbons];
-                if(hydrogens == (carbons*2)+2){
-                    ans = ans + "ane";
-                }
-                else if(hydrogens == carbons*2){
-                    ans = ans+"ene";
-                }
-                else if(hydrogens == (carbons*2)-2 && hydrogens!=0){
-                    ans = ans+"yne";
+                if(oxygens == 0) {
+                    if (hydrogens == (carbons * 2) + 2) {
+                        ans = ans + "ane";
+                    }
+                    else if (hydrogens == carbons * 2 && carbons > 1) {
+                        ans = ans + "ene";
+                    }
+                    else if (hydrogens == (carbons * 2) - 2 && hydrogens != 0) {
+                        ans = ans + "yne";
+                    }
                 }
                 answer.setVisibility(View.VISIBLE);
                 answer.setText(ans);
+            }
+        });
 
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i = 0;i<3;i++){
+                    for(int j = 0;j<10;j++){
+                        temp = "cell"+i+j;
+                        resid = getResources().getIdentifier(temp,"id", getPackageName());
+                        EditText t = findViewById(resid);
+                        t.setText("");
+                        answer.setVisibility(View.INVISIBLE);
+                    }
+                }
             }
         });
     }
